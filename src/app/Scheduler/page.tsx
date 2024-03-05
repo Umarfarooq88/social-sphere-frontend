7f7dcb16c90b05cecf9034a8d7ba626ce6d22186
Umar farooq <umarat372@gmial.com>
2024-03-05 20:53:59 +0530

    Added scheduler chart

---
+++ src/app/Scheduler/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "@/components/Navbar";
+import Scheduler from "@/components/Scheduler";
+
+const Page = () => {
+  return (
+    <div>
+      <Navbar /> 
+      <Scheduler /> 
+    </div>
+  );
+};
+
+export default Page;
