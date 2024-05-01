2ef8951de7e416fd5367a31e03595e990fd34d7d
Tahmeerpasha <tahmeerpashacjc@gmail.com>
2024-05-01 17:18:40 +0530

    Modify: Refactor the code

--- src/app/(main)/account/page.tsx
+++ src/app/(main)/account/page.tsx
@@ -1,8 +1,8 @@
 "use client";
-import Sidebar from "@/components/account/Sidebar";
 import { isUserLoggedIn } from "@/lib/utils/utils";
 import React, { useEffect, useState } from "react";
 import Account from "@/components/account/Account";
+import Sidebar from "@/components/account/Sidebar";
 import Channel from "@/components/account/Channel";
 const Page = () => {
   const [page, setPage] = useState("Account");
@@ -26,6 +26,7 @@ const Page = () => {
       <Sidebar
         className={"left-0 pt-10 h-screen absolute top-16"}
         setPage={setPage}
+        selectedPage={page}
       />
       <div className="absolute left-80">{render()}</div>
     </div>
